b9ead97e4e1231ac558eae5b48f21601ab37cede
Devin Dow <DevinDow@gmail.com>
2018-09-21 13:27:37 -0700

    "rails g scaffold Meal time:time, foods:string, day:references", shallow nesting, Day has_many: meals, "rails db:migrate"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :days
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
+
+  resources :days do
+    resources :meals, shallow: true
+  end
 
 end

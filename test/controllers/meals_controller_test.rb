d31ee04700db94ff7af5882e90e2107f5aa197b2
Devin Dow <DevinDow@gmail.com>
2018-09-21 21:45:05 -0700

    Tests PASS with better member variable names

--- test/controllers/meals_controller_test.rb
+++ test/controllers/meals_controller_test.rb
@@ -2,47 +2,47 @@ require 'test_helper'
 
 class MealsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @day = days(:day1)
-    @meal = meals(:breakfast)
+    @day1 = days(:day1)
+    @breakfast = meals(:breakfast)
   end
 
   test "should get index" do
-    get day_meals_url(@meal.day)
+    get day_meals_url(@breakfast.day)
     assert_response :success
   end
 
   test "should get new" do
-    get new_day_meal_url(@day)
+    get new_day_meal_url(@day1)
     assert_response :success
   end
 
   test "should create meal" do
     assert_difference('Meal.count') do
-      post day_meals_url(@meal.day), params: { meal: { foods: @meal.foods, time: @meal.time } }
+      post day_meals_url(@breakfast.day), params: { meal: { foods: @breakfast.foods, time: @breakfast.time } }
     end
 
-    assert_redirected_to day_url(@meal.day)
+    assert_redirected_to day_url(@breakfast.day)
   end
 
   test "should show meal" do
-    get meal_url(@meal)
+    get meal_url(@breakfast)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_meal_url(@meal)
+    get edit_meal_url(@breakfast)
     assert_response :success
   end
 
   test "should update meal" do
-    patch meal_url(@meal), params: { meal: { day_id: @meal.day_id, foods: @meal.foods, time: @meal.time } }
-    assert_redirected_to day_url(@meal.day)
+    patch meal_url(@breakfast), params: { meal: { foods: @breakfast.foods, time: @breakfast.time } }
+    assert_redirected_to day_url(@breakfast.day)
   end
 
   test "should destroy meal" do
-    day = @meal.day
+    day = @breakfast.day
     assert_difference('Meal.count', -1) do
-      delete meal_url(@meal)
+      delete meal_url(@breakfast)
     end
 
     assert_redirected_to day_url(day)

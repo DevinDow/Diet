4bf3484fbe4b478734ff2cd10e7f008a6dc7119f
Devin Dow (Mac) <DevinDow@gmail.com>
2018-09-24 11:29:24 -0700

    added other categories to Meal show & edit

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -13,4 +13,24 @@ class Meal < ApplicationRecord
     (categories && categories[1]) || 0
   end
 
+  def proteins
+    (categories && categories[2]) || 0
+  end
+
+  def carbs
+    (categories && categories[3]) || 0
+  end
+
+  def fats
+    (categories && categories[4]) || 0
+  end
+
+  def seeds
+    (categories && categories[5]) || 0
+  end
+
+  def oils
+    (categories && categories[6]) || 0
+  end
+
 end

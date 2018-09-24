ce44dd07d21aa5a96d956d81306d81c8a08cccdd
Devin Dow (Mac) <DevinDow@gmail.com>
2018-09-24 15:35:35 -0700

    enclose each <input> tag in a table cell <td> </td>

--- app/inputs/array_input.rb
+++ app/inputs/array_input.rb
@@ -3,13 +3,20 @@ class ArrayInput < SimpleForm::Inputs::StringInput
   def input(wrapper_option)
     input_html_options[:type] ||= input_type
 
-    Array(object.public_send(attribute_name)).map do |array_el|
+    # build array of <input> tags for the array
+    input_tags = Array(object.public_send(attribute_name)).map do |array_elment|
+      # build each <input> tag for each array element 
       @builder.text_field(nil, 
-                          input_html_options.merge(value: array_el, 
-                                                   name: "#{object_name}[#{attribute_name}][]", 
+                          input_html_options.merge(value: array_elment,
+                                                   name: "#{object_name}[#{attribute_name}][]",
                                                    step: 0.5,
                                                    style: "width: 50px"))  # this makes the <input style="width:50px">
-    end.join.html_safe
+    end
+
+    # enclose each <input> tag in a table cell <td> </td>
+    input_tags.map do |input_tag|
+      "<td>" + input_tag + "</td>"
+    end.join.html_safe  # join the html of the map of <td><input></td>
   end
 
   def input_type

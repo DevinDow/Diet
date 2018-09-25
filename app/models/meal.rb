2538c184ac40048a913593dacedeb112334ed4d1
Devin Dow (Mac) <DevinDow@gmail.com>
2018-09-24 20:28:03 -0700

    jettison fields for categories

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,32 +5,4 @@ class Meal < ApplicationRecord
     time.strftime("%I:%M%p %Z")
   end
 
-  def veggies
-    (categories && categories[0]) || 0
-  end
-
-  def fruits
-    (categories && categories[1]) || 0
-  end
-
-  def proteins
-    (categories && categories[2]) || 0
-  end
-
-  def carbs
-    (categories && categories[3]) || 0
-  end
-
-  def fats
-    (categories && categories[4]) || 0
-  end
-
-  def seeds
-    (categories && categories[5]) || 0
-  end
-
-  def oils
-    (categories && categories[6]) || 0
-  end
-
 end

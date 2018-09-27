f4ec6a6532d2522a9f79a421de01d92eaa52a76c
Devin Dow <DevinDow@gmail.com>
2018-09-27 12:41:03 -0700

    converting time (minutes since midnight) to a Time object

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -3,6 +3,15 @@ class Meal < ApplicationRecord
 
   def time_of_day
     Time.new.at_midnight.advance(minutes: time).strftime("%I:%M%p")
+    Time.new.at_midnight.advance(minutes: time || 0)
+  end
+
+  def time_of_day=(val)
+    @time = (val - Time.new.at_midnight) / 60
+  end
+
+  def time_string
+    time_of_day.strftime("%I:%M%p")
   end
 
 end

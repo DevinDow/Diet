89bdb5b60096d3ed6b7dd744c598c69d09cfbb50
Devin Dow <DevinDow@gmail.com>
2018-10-15 16:33:20 -0700

    Meals in container on one line

--- app/javascript/Meal.vue
+++ app/javascript/Meal.vue
@@ -1,16 +1,18 @@
 <template>
-  <form v-if="editing" class="meal" @submit.prevent="$emit('update', meal_edit); editing=false" @keydown.esc="editing=false">
-    <input class="minutes_since_midnight" type="number" v-model="meal_edit.minutes_since_midnight" />
-    <input class="foods" ref="foods" v-model="meal_edit.foods" />
-    <button type="submit">Submit</button>
-    <button type="button" @click="editing=false">Cancel</button>
-    <button type="button" @click="$emit('delete', meal_edit)" data-confirm="Are you sure?">Delete</button>
-  </form>
-  <div v-else v-bind="attrs">
-    <div class="minutes_since_midnight">Time = {{ meal.minutes_since_midnight }}</div>
-    <div class="foods">Foods = {{ meal.foods }}</div>
-    <button @click="startEditing">Edit</button>
-    <button @click="$emit('delete', meal)" data-confirm="Are you sure?">Delete</button>
+  <div class="meal container">
+    <form v-if="editing" class="meal" @submit.prevent="$emit('update', meal_edit); editing=false" @keydown.esc="editing=false">
+      <input class="minutes_since_midnight" type="number" v-model="meal_edit.minutes_since_midnight" />
+      <input class="foods" ref="foods" v-model="meal_edit.foods" />
+      <button type="submit">Submit</button>
+      <button type="button" @click="editing=false">Cancel</button>
+      <button type="button" @click="$emit('delete', meal_edit)" data-confirm="Are you sure?">Delete</button>
+    </form>
+    <div v-else v-bind="attrs">
+      <span class="minutes_since_midnight">Time = {{ meal.minutes_since_midnight }}</span>
+      <span class="foods">Foods = {{ meal.foods }}</span>
+      <button @click="startEditing">Edit</button>
+      <button @click="$emit('delete', meal)" data-confirm="Are you sure?">Delete</button>
+    </div>
   </div>
 </template>
 

a0ff21ecccff6b988a4fed588520d36afbb6571f
Devin Dow <DevinDow@gmail.com>
2018-10-16 07:40:06 -0700

    showing totals

--- app/javascript/Meals.vue
+++ app/javascript/Meals.vue
@@ -17,6 +17,15 @@
         v-bind:key="meal.id"
         v-on:update="onUpdateMeal"
         v-on:delete="onDeleteMeal" />
+      <td class="time total">TOTALS</td>
+      <td class="foods"></td>
+      <td class="category veggies total">{{ day.totals[0] }}</td>
+      <td class="category fruits total">{{ day.totals[1] }}</td>
+      <td class="category proteins total">{{ day.totals[2] }}</td>
+      <td class="category carbs total">{{ day.totals[3] }}</td>
+      <td class="category fats total">{{ day.totals[4] }}</td>
+      <td class="category seeds total">{{ day.totals[5] }}</td>
+      <td class="category oils total">{{ day.totals[6] }}</td>
     </table>
     <NewMeal v-on:create="onCreateMeal" />
   </div>

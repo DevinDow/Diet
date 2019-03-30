f4d61c0d45511451e6bda3cbe0004970fa2a50ec
Devin Dow <DevinDow@gmail.com>
2019-03-30 06:34:16 -0700

    Temporary Improvement: output table of times to minutes

--- app/javascript/App.vue
+++ app/javascript/App.vue
@@ -2,7 +2,30 @@
   <div id="today">
     <button @click="fetchYesterday">Yesterday</button>
     <Day v-if="day" v-bind:day="day" />
+  
+    <div>
+      6am  = 360 <br/>
+      7am  = 420 <br/>
+      8am  = 480 <br/>
+      9am  = 540 <br/>
+      <br/>
+      10am = 600 <br/>
+      11am = 660 <br/>
+      noon = 720 <br/>
+      <br/>
+      1pm  = 780 <br/>
+      2pm  = 840 <br/>
+      3pm  = 900 <br/>
+      4pm  = 960 <br/>
+      5pm  = 1020 <br/>
+      <br/>
+      6pm  = 1080 <br/>
+      7pm  = 1140 <br/>
+      8pm  = 1200 <br/>
+      9pm  = 1260 <br/>
+    </div>
   </div>
+
 </template>
 
 <script>

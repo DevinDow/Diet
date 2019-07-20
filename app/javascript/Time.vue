cf92ce37a58b9a94ebb8320ad4796780f0e3a31b
Devin Dow <DevinDow@gmail.com>
2019-07-20 12:09:16 -0700

    calling APIs.fetchTime()

--- app/javascript/Time.vue
+++ app/javascript/Time.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="time">
+    <button @click="pingTime">Ping Time</button>
     Time = 
+    <span v-if="pinging">pinging...</span>
   </div>
 </template>
 
@@ -10,6 +12,20 @@ export default {
   mounted: function () {
     console.log("**Time MOUNTED**")
     console.log(this)
+  },
+
+  data: function () {
+    return {
+      pinging: false
+    }
+  },
+
+  methods: {
+    pingTime() {
+      console.log("Pinging Time")
+      this.pinging = true
+      APIs.fetchTime()
+    }
   }
 }
 </script>
